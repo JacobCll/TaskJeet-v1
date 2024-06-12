64038b5ea33da574eeab3cb1fe0b8dd52cb534ec
JacobCll <jacobcarbonell06@gmail.com>
2024-06-12 18:36:55 +0800

    Tic-tac-toe react tutorial

--- learning-app/src/App.tsx
+++ learning-app/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+let idCounter = 0;
+// list of jsx quotes
+
+export default function Quotes() {
+  const [name, setName] = useState("");
+  const [quotes, setQuotes] = useState([]);
+
+  let quotes_list = [];
+
+  const clearQuote = (id: any) => {
+    setQuotes(quotes.filter((quote) => quote.id !== id));
+  };
+
+  // iterate each quote and turn it into jsx
+  quotes.forEach((quote: any) => {
+    if (quote.author.toLowerCase().indexOf(name.toLowerCase()) === -1) {
+      return;
+    }
+
+    quotes_list.push(
+      <div className="quote-box">
+        <div className="highlighted">
+          <h4 className="quote-quote">"{quote.quote}"</h4>
+          <p className="quote-author">{quote.author}</p>
+          <button className="clear-quote" onClick={() => clearQuote(quote.id)}>
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToolBar
+        quotes={quotes}
+        setQuotes={setQuotes}
+        name={name}
+        setName={setName}
+      />
+
+      <div className="quotes-collection">{quotes_list}</div>
     </>
-  )
+  );
 }
 
-export default App
+function ToolBar({ quotes, setQuotes, name, setName }) {
+  const getQuote = () => {
+    axios
+      .get("api/random")
+      .then((response) => {
+        const quote = response.data[0].q;
+        const author = response.data[0].a;
+        console.log(response.data[0]);
+
+        setQuotes([
+          ...quotes,
+          {
+            id: idCounter++,
+            quote: quote,
+            author: author,
+          },
+        ]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="toolbar">
+      <h1 className="app-title ">Quote List</h1>
+      <input
+        value={name}
+        placeholder="Search Author..."
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <div className="quote-buttons">
+        <button className="get-quote-button" onClick={getQuote}>
+          New Quote
+        </button>
+        <button
+          onClick={() => {
+            setQuotes([]);
+          }}
+        >
+          Clear All
+        </button>
+      </div>
+    </div>
+  );
+}
--- learning-app/vite.config.ts
+++ learning-app/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: true,
+    proxy: {
+      '/api/random': {
+        target: 'https://zenquotes.io/api/random',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
